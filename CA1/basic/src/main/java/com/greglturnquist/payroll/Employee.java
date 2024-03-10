e80c2790a5fad6e4b406af283e2cbad445e5484b
Solange-o <1212208@isep.ipp.pt>
2024-03-10 00:45:10 +0000

    [Refactor] #5 - Add support for the email field.

--- CA1/basic/src/main/java/com/greglturnquist/payroll/Employee.java
+++ CA1/basic/src/main/java/com/greglturnquist/payroll/Employee.java
@@ -35,24 +35,27 @@ public class Employee {
     private String description;
     private String jobTitle;
     private int jobYears;
+    private String email;
 
     private Employee() {
     }
 
-    public Employee(String firstName, String lastName, String description, String jobTitle, int jobYears) {
+    public Employee(String firstName, String lastName, String description, String jobTitle, int jobYears, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.description = description;
         this.jobTitle = jobTitle;
         this.jobYears = jobYears;
+        this.email = email;
     }
 
-    public boolean validateArguments(String firstName, String lastName, String description, String jobTitle, int jobYears) {
+    public boolean validateArguments(String firstName, String lastName, String description, String jobTitle, int jobYears, String email) {
         if (firstName == null || firstName.trim().isEmpty()) return false;
         if (lastName == null || lastName.trim().isEmpty()) return false;
         if (description == null || description.trim().isEmpty()) return false;
         if (jobTitle == null || jobTitle.trim().isEmpty()) return false;
         if (jobYears < 0) return false;
+        if (email == null || email.trim().isEmpty()) return false;
         return true;
     }
 
@@ -66,13 +69,14 @@ public class Employee {
                 Objects.equals(lastName, employee.lastName) &&
                 Objects.equals(description, employee.description) &&
                 Objects.equals(jobTitle, employee.jobTitle) &&
-                Objects.equals(jobYears, employee.jobYears);
+                Objects.equals(jobYears, employee.jobYears) &&
+                Objects.equals(email, employee.email);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(id, firstName, lastName, description, jobTitle, jobYears);
+        return Objects.hash(id, firstName, lastName, description, jobTitle, jobYears, email);
     }
 
     public Long getId() {
@@ -107,20 +111,28 @@ public class Employee {
         this.description = description;
     }
 
-    public void getJobTitle(String jobTitle) {
-        this.jobTitle = jobTitle;
+    public String getJobTitle() {
+        return jobTitle;
     }
 
     public void setJobTitle(String jobTitle) {
         this.jobTitle = jobTitle;
     }
 
-    public void getJobYears(int jobYears) {
-        this.jobYears = jobYears;
+    public int getJobYears() {
+        return jobYears;
     }
 
     public void setJobYears(int jobYears) {
         this.jobYears = jobYears;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Override
@@ -132,6 +144,7 @@ public class Employee {
                 ", description='" + description + '\'' +
                 ", jobTitle='" + jobTitle + '\'' +
                 ", jobYears='" + jobYears + '\'' +
+                ", email='" + email + '\'' +
                 '}';
     }
 
